77a05d6a4a8800aafc8bae23cf621870d234cc36
unknown <petter.fogel@medieinstitutet.se>
2020-10-25 12:30:30 +0100

    added logic to hamburger menu slide

---
+++ script.js
@@ -0,0 +1,10 @@
+function openBurgerMenu() {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links")
+    
+    burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active")
+ });
+}
+
+
